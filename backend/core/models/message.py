999c739c8479dc5389cdff5f24fbe4ab0be92bfc
Anish Ghimire <anishghimire862@gmail.com>
2026-03-23 22:36:52 +0545

    feat: order messages by created at date

--- backend/core/models/message.py
+++ backend/core/models/message.py
@@ -19,5 +19,8 @@ class Message(models.Model):
     model = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['created_at']
+
     def __str__(self):
         return f"Message {self.uuid} in ChatRoom {self.chatroom.name}"
