359df8e1e06cfeb357cb2a14a5bb84a669d11e85
Krrish Ghimire <krrish.ghimire@amniltech.com>
2026-04-04 11:22:56 +0545

    close socket

--- frontend/components/ui/progress/index.ts
+++
@@ -1,6 +0,0 @@
-import { ProgressRoot, ProgressIndicator } from 'reka-ui'
-
-const Progress = ProgressRoot
-const ProgressIndicatorComponent = ProgressIndicator
-
-export { Progress, ProgressIndicatorComponent as ProgressIndicator }
